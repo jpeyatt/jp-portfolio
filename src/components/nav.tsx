f4429f28c7740f969d9bf07dea77b04651804bef
Justin Peyatt <justinpeyatt@gmail.com>
2021-07-13 20:06:26 -0400

    Add mobile menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,10 +3,22 @@ import '../styles/nav.css';
 
 const Nav: FunctionComponent = () => {
     const [navClass, setNavClass] = useState('at-top');
+    const mobileMenuBtnEl = document.querySelector('#mobile-menu-button');
+    const linksEl = document.querySelector('#links');
 
     const handleScroll = () => {
         if (window.scrollY > 80) setNavClass('not-at-top');
         else setNavClass('at-top');
+    }
+
+    const onMenuBtnClick = () => {
+        if (mobileMenuBtnEl?.classList.contains('open')) {
+            mobileMenuBtnEl?.classList.remove('open');
+            linksEl?.classList.remove('open');
+        } else {
+            mobileMenuBtnEl?.classList.add('open');
+            linksEl?.classList.add('open');
+        }
     }
 
     useEffect(() => {
@@ -18,7 +30,12 @@ const Nav: FunctionComponent = () => {
             <ul className="brand-logo">
                 <li>JP.</li>
             </ul>
-            <ul className="link-wrap">
+            <button id="mobile-menu-button" onClick={onMenuBtnClick} className="mobile-menu">
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+            </button>
+            <ul id="links" className="links">
                 <li><a href="/">About Me</a></li>
                 <li><a href="/">Projects</a></li>
                 <li><a href="/">Resume</a></li>
