035fe22852a7c7f86a0cd697b1e146a6cfab7088
Justin Peyatt <j.peyatt@criteo.com>
2021-09-01 18:13:46 -0400

    Create Project data and update Project model.

---
+++ src/data/projects.data.tsx
@@ -0,0 +1,14 @@
+import { IProject } from '../models/project.model';
+
+export const ProjectsData: IProject[] = [
+    {
+        id: 1,
+        title: '',
+        description: '',
+        screenshot: '',
+        stack: [],
+        github_url: '',
+        demo_url: ''
+    }
+];
+
--- src/models/project.model.tsx
+++ src/models/project.model.tsx
@@ -2,6 +2,7 @@ export interface IProject {
     id: string;
     title: string;
     description: string;
+    screenshot: string;
     stack: string[];
     github_url: string;
     demo_url: string;
